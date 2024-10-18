18990ed648894a8e20a3534429cd1e00800732e8
babafemij-k <olaolujkb21@gmail.com>
2024-10-18 21:27:37 +0100

    Update blog post

--- components/blog/react-state-mgt/WeeklyFrontendGoals.tsx
+++ components/blog/react-state-mgt/WeeklyFrontendGoals.tsx
@@ -13,16 +13,32 @@ const WeeklyFrontendGoals = () => {
     }
   };
 
+  const clearGoals = () => {
+    setGoalsList([]);
+  };
+
   return (
     <div className="bg-white max-w-sm mx-auto p-4 lg:p-6 lg:max-w-3xl lg:ml-0 rounded-lg border-2 border-gray-300">
-      <h3 className="text-lg font-semibold">Weekly Frontend Goals</h3>
+      <h3 className="text-lg font-semibold mb-20">Weekly Frontend Goals</h3>
       <input
         type="text"
         value={goal}
         onChange={(e) => setGoal(e.target.value)}
         placeholder="Enter your goal"
+        className="w-full p-2 border border-gray-300 rounded-md"
       />
-      <button onClick={addGoal}>Add to list</button>
+      <button
+        onClick={addGoal}
+        className="bg-slate-700 text-white px-4 py-2 mt-4 transform transition-transform duration-200 active:scale-95 mr-4"
+      >
+        Add to list
+      </button>
+      <button
+        onClick={clearGoals}
+        className="bg-red-500 text-white px-4 py-2 mt-4 transform transition-transform duration-200 active:scale-95"
+      >
+        Clear list
+      </button>
       <ul>
         {goalsList.map((item, index) => (
           <li key={index}>{item}</li>
