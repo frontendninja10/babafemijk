dc8b08193bb1c81d29fe0ce54f7900d7e9c566b5
babafemij-k <olaolujkb21@gmail.com>
2024-10-22 19:02:41 +0100

    Redesign Footer component

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,20 +1,35 @@
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="border-t border-zinc-300 mt-44">
       <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
-        <small className=" duration-200 font-mono">
-          All rights reserved &copy; {new Date().getFullYear()}
-        </small>
-
-        <small>
-          <a
-            href="https://github.com/Evavic44/sanity-nextjs-site"
+        <div>
+          <p>Babafemi J.K.</p>
+        </div>
+        <div className="flex space-x-4">
+          <Link
+            href="https://github.com/frontendninja10"
             target="_blank"
-            rel="noreferrer noopener"
+            rel="noopener noreferrer"
           >
-            I was inspired by <span className="text-green-400">Victor Eke</span>
-          </a>
-        </small>
+            GitHub
+          </Link>
+          <Link
+            href="https://twitter.com/frontendninja10"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            X
+          </Link>
+          <Link
+            href="https://linkedin.com/in/babafemijk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </Link>
+          <Link href="mailto:babafemijk@gmail.com">Contact</Link>
+        </div>
       </div>
     </footer>
   );
